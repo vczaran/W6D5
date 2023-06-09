b83eefe05d407748d3d489bb4429fa6d3e567939
mxcrpntr <carpenter.max@gmail.com>
2023-06-09 17:13:21 -0400

    rendered index successfully

--- 99cats/ninety_nine_cats/app/controllers/cats_controller.rb
+++ 99cats/ninety_nine_cats/app/controllers/cats_controller.rb
@@ -1,8 +1,8 @@
 class CatsController < ApplicationController
 
     def index
-        cats = Cat.all
-        render json: cats
+        @cats = Cat.all
+        render :index
     end
 
     def show
@@ -11,22 +11,36 @@ class CatsController < ApplicationController
     end
 
     def create
-        cat = Cat.new
+        cat = Cat.new(cat_params)
+        if cat.save
+            render json: cat
+        else
+            render json: cat.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def update
+        cat = Cat.find_by(id: params[:id])
+        if cat.update(cat_params)
+            redirect_to cat_url(cat.id)
+        else
+            render json: cat.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def new
+        render :new
     end
 
     def edit
+        @cat = Cat.find_by(id: params[:id])
+        render :edit
     end
 
     private
 
     def cat_params
-
+        params.require(:cat).permit(:name,:sex,:birth_date,:color,:description)
     end
     
 end

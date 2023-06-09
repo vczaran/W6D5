38325f1e17aaa580e343d974d12d81540d799e04
Viktoria Czaran <vczaran@gmail.com>
2023-06-09 17:31:24 -0400

    Completed anchor tags in index and show

--- 99cats/ninety_nine_cats/app/controllers/cats_controller.rb
+++ 99cats/ninety_nine_cats/app/controllers/cats_controller.rb
@@ -6,8 +6,8 @@ class CatsController < ApplicationController
     end
 
     def show
-        cat = Cat.find_by(id: params[:id])
-        render json: cat
+        @cat = Cat.find_by(id: params[:id])
+        render :show
     end
 
     def create
